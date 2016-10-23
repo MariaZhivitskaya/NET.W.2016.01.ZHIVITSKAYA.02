419cd231189065956f8546e62c366522c1e6b207
Masha <Masha>
2016-10-23 17:00:03 +0300

    Add repository setting files

---
+++ ClassLibraryTask1StrongName/City.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassLibraryTask1StrongName
+{
+    public class City
+    {
+        public string Name
+        {
+            set;
+            get;
+        }
+        public float Area
+        {
+            set;
+            get;
+        }
+
+        public City(string name, float area)
+        {
+            this.Name = name;
+            this.Area = area;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", area: " + Area + " square kilometres";
+        }
+    }
+}
---
+++ ConsoleApplicationTask1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibraryTask1;
+
+namespace ConsoleApplicationTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var city1 = new City("Minsk", 307.9f);
+            var city2 = new City("London", 1572.0f);
+
+            Console.WriteLine(city1.ToString());
+            Console.WriteLine(city2.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
